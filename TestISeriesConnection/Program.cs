1e50ce284795fcd65a656ee55797364fa015ecb7
mmammadov <mmammadov@griddynamics.com>
2022-12-14 13:51:52 +0400

    Add project files.

---
+++ TestISeriesConnection/Program.cs
@@ -0,0 +1,22 @@
+﻿using IBM.Data.DB2.iSeries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestISeriesConnection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Connecting...");
+            var connection = new iDB2Connection("DataSource=10.10.149.104;UserID=#MOBILEAPI;Password=TAMPAB#2; Default Collection=EXPRESS; DataCompression=True; AllowUnsupportedChar=True");
+            connection.Open();
+            Console.WriteLine("Connected");
+            connection.Close();
+            Console.ReadKey();
+        }
+    }
+}
